eeef59ebd9e36781b780c40245d4a445dc29d89f
Stefan Laufmann <stefan.laufmann@mailbox.tu-berlin.de>
2018-08-08 16:02:55 +0200

    added println's to Actions for debugging

--- src/main/kotlin/editor/Actions.kt
+++ src/main/kotlin/editor/Actions.kt
@@ -5,24 +5,28 @@ import javax.swing.AbstractAction
 
 class UndoAction(val scene: Viewport) : AbstractAction() {
     override fun actionPerformed(p0: ActionEvent?) {
+        println("UndoAction")
         scene.popOperation()
     }
 }
 
 class RedoAction(val scene: Viewport) : AbstractAction() {
     override fun actionPerformed(p0: ActionEvent?) {
+        println("RedoAction")
         scene.popReverseOperation()
     }
 }
 
 class PressSpaceAction(val scene: Viewport) : AbstractAction() {
     override fun actionPerformed(p0: ActionEvent?) {
+        println("Space!")
         scene.spaceBarPressed = true
     }
 }
 
 class ReleaseSpaceAction(val scene: Viewport) : AbstractAction() {
     override fun actionPerformed(p0: ActionEvent?) {
+        println("No Space.")
         scene.spaceBarPressed = false
     }
 }
