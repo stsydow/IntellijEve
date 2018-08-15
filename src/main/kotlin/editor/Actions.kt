4c7561c0638f386b5841a5e7dd7cf9dd3fe8b80c
Stefan Laufmann <stefan.laufmann@mailbox.tu-berlin.de>
2018-08-15 15:07:51 +0200

    minor change to reduce debug spam

--- src/main/kotlin/editor/Actions.kt
+++ src/main/kotlin/editor/Actions.kt
@@ -5,14 +5,12 @@ import javax.swing.AbstractAction
 
 class UndoAction(val scene: Viewport) : AbstractAction() {
     override fun actionPerformed(p0: ActionEvent?) {
-        println("UndoAction")
         scene.popOperation()
     }
 }
 
 class RedoAction(val scene: Viewport) : AbstractAction() {
     override fun actionPerformed(p0: ActionEvent?) {
-        println("RedoAction")
         scene.popReverseOperation()
     }
 }
